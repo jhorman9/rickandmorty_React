26ca2c2a1d97b8ae912d7cfa5c21f0f4515ffe44
jhorman9 <jhosmita9@gmail.com>
2022-11-06 21:46:43 -0500

    actualizar

--- component/Character.jsx
+++ component/Character.jsx
@@ -31,11 +31,25 @@ const Character = ({location}) => {
             </div>
             <div className='info'>
                     {residentInfo.status === "Alive" ? (
-                        <div className='row'> <div className='status' style={{background:"green", boxShadow:"0 0 10px green"}}></div> {residentInfo.status} - {residentInfo.species}</div>
+                        <div className='row-card-info'> 
+                            <div className='status-content'>
+                                <div className='status' style={{background:"green", boxShadow:"0 0 10px green"}}></div>
+                            </div>
+                            <div className='status-info'>
+                                <p>{residentInfo.status} - {residentInfo.species}</p> 
+                            </div>
+                        </div>
                     ) : residentInfo.status === "Dead" ? (
-                        <div className='row'> <div className='status' style={{background:"red", boxShadow:"0 0 10px red"}}></div> {residentInfo.status} - {residentInfo.species}</div>
+                        <div className='row-card-info'> 
+                            <div className='status-content'>
+                                <div className='status' style={{background:"red", boxShadow:"0 0 10px red"}}></div>
+                            </div>
+                            <div className='status-info'>
+                                <p>{residentInfo.status} - {residentInfo.species}</p> 
+                            </div>
+                        </div>
                     ) : (
-                        <div className='row'> <div className='status' style={{background:"gray", boxShadow:"0 0 10px gray"}}></div> {residentInfo.status} - {residentInfo.species}</div>
+                        <div className='row-card-info'> <div className='status' style={{background:"gray", boxShadow:"0 0 10px gray"}}></div> {residentInfo.status} - {residentInfo.species}</div>
                     )}
                 <p><b>Name:</b> <span>{residentInfo.name}</span></p>
                 <p><b>Origin: </b> <span>{residentInfo.origin?.name}</span></p>
